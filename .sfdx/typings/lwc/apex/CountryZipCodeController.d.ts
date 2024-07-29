4a4b8001782379a6ba1fac0b31140c5446cb62c4
Biswajit Barman <bbarman@apus.edu>
2024-07-29 21:27:27 +0530

    Task

---
+++ .sfdx/typings/lwc/apex/CountryZipCodeController.d.ts
@@ -0,0 +1,3 @@
+declare module "@salesforce/apex/CountryZipCodeController.getCountryByZipCode" {
+  export default function getCountryByZipCode(param: {zipCode: any, countryCode: any}): Promise<any>;
+}
